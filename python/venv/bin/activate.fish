239c89d93c6a8d70096611bbdaa54cb0d7b11e31
Hope Tambala <hopetambala@outlook.com>
2019-02-01 18:31:13 -0500

    venv

--- python/venv/bin/activate.fish
+++ python/venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/hopetambala/Documents/development/work/libviz/threeJS-NCAAB/python/venv"
+set -gx VIRTUAL_ENV "/Users/hopetambala/Documents/development/work/libviz/dexJS-NCAAB/python/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
