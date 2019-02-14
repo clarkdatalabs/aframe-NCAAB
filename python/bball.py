a676305202593dde2065f217ac485ec3b68bf663
Hope Tambala <hopetambala@outlook.com>
2019-02-14 16:39:03 -0500

    fix(): data

--- python/bball.py
+++ python/bball.py
@@ -12,30 +12,55 @@ ncaa_basketball = bq_helper.BigQueryHelper(active_project="bigquery-public-data"
 # List of all the tables in the ncaa_basketball dataset
 tables = ncaa_basketball.list_tables()
 
-# Chosen Data Set
-# mbb_teams_games_sr
+
+#TWO REQUIRED DATASETS
+#mbb_historical_teams_seasons
+#mbb_teams
 
 # Query of All Data from Teams Games
 old_query = """SELECT * 
             FROM `bigquery-public-data.ncaa_basketball.mbb_teams_games_sr`"""
 
+old_query2 = """
+    SELECT 
+        team_code,
+        season,
+        market, 
+        name, 
+        alias, 
+        current_division, 
+        wins, 
+        losses,
+        ties,
+        (wins / losses) AS pct
+    FROM 
+        `bigquery-public-data.ncaa_basketball.mbb_historical_teams_seasons`
+    WHERE 
+        2013 = season;
+"""
+
 query = """
-SELECT 
-    season,
-    market, 
-    name, 
-    alias, 
-    current_division, 
-    wins, 
-    losses,
-    ties,
-    (wins / losses) AS pct
-FROM `bigquery-public-data.ncaa_basketball.mbb_historical_teams_seasons`
-WHERE 2013 = season;
+    SELECT 
+        season,
+        round,
+        win_seed,
+        win_market,
+        win_name,
+        win_alias,
+        win_school_ncaa,
+        win_pts,
+        lose_seed,
+        lose_market,
+        lose_name,
+        lose_school_ncaa,
+        lose_pts
+    FROM `bigquery-public-data.ncaa_basketball.mbb_historical_tournament_games` as teams_tournament
+    WHERE 2013 = season;
 """
 
 df = ncaa_basketball.query_to_pandas_safe(query, max_gb_scanned=1)
-df.to_csv(r'generated_data/'+'2013_season'+'.csv')
+#df.to_csv(r'generated_data/'+'2013_season'+'.csv')
+df.to_csv('2013_season'+'.csv')
 
 
 # Print 10000 Records of Each Table
