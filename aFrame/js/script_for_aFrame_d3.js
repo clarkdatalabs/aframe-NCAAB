c712c0bd05b98b492b1d111866a4b3d57c12ce51
neilzhu2 <neilzhu@umich.edu>
2019-03-01 17:23:36 -0500

    vertical self curve done, but interaction has problem

--- aFrame/js/script_for_aFrame_d3.js
+++ aFrame/js/script_for_aFrame_d3.js
@@ -26,110 +26,227 @@
                       .range([-15, -10, -5, 0, 5, 10, 15]);
 
 
-// function to get the uniquename of a team
-    const getName = (data) => data.market + ' ' + data.name;
+// FUNCTION to get the uniquename of a team
+    const getName = (data) => {
+        let usableMarketName = data.market.replace(' ', '_'),
+            usableTeamName =  data.name.replace(' ', '_')
+        return usableMarketName + '_' + usableTeamName;
+    }
 
 
-    d3.csv('2017_season_detailed_cleaned.csv').then(function(data){
-        var points = [], // to collect all points
-            teams = {}, // to have each unique team collected and assigne teamIndex
-            teamId = 0,
-            gameTeamIdPositions = {}; // get the pairs of id (in csv) & point position
 
+
+d3.csv('2017_season_detailed_cleaned.csv').then(function(data){
+    var points = [], // 1.0 to collect points get by all games, for min/max ponits
+        teams = {}, // 2.0 {teamUniqueName: teamIndex} to have each unique team collected and assigned teamIndex
+        teamIndex = 0, // 2.1 teamIndex, this is to assign each team on specific angels
+        gameTeamIdPositions = {}; // 3.0 {id: point, id: point, ...} get the pairs of id (in csv) & point position, This is for lines between opposite teams
+        teamsSelfPositions = {}; // 2.2 {teamName:[points], ...} This is for curves among themselves through rounds
+
+    // assigne teamIndex to each team named teamUniqueName
         data.forEach((d)=>{
             points.push(+d.points_game);
 
             let teamName = getName(d);
-            if(! teams.hasOwnProperty(teamName)) teams[teamName] = teamId++;
+            if(! teams.hasOwnProperty(teamName)) teams[teamName] = teamIndex++;
         })
 
-        // parameters that need csv
-        const maxPoint = Math.max(...points),
-              minPoint = Math.min(...points);
+    // parameters that need csv
+    const maxPoint = Math.max(...points),
+          minPoint = Math.min(...points);
 
-        const r_scale = d3.scaleLinear()
-                              .domain([minPoint, maxPoint])
-                              .range([maxR, 0]);
+    const r_scale = d3.scaleLinear()
+                          .domain([minPoint, maxPoint])
+                          .range([maxR, 0]);
 
-        // function to position a team at a round
-        const teamPlace = (teamIndex, score, round) => {
-            let this_r = r_scale(score);
-            let x_position = Math.cos(angleInterval * teamIndex) * this_r,
-                y_position = y_scale(round),
-                z_position = Math.sin(angleInterval * teamIndex) * this_r;
-            return(`${x_position} ${y_position} ${z_position}`);
-        };
+    // FUNCTION to position a team at a round
+    const teamPlace = (teamIndex, score, round) => {
+        let this_r = r_scale(score);
+        let x_position = Math.cos(angleInterval * teamIndex) * this_r,
+            y_position = y_scale(round),
+            z_position = Math.sin(angleInterval * teamIndex) * this_r;
+        return(`${x_position} ${y_position} ${z_position}`);
+    };
 
 
-        // create the spheres as teams
-            aEntity.selectAll('.team')
+    // create the spheres as teams
+        aEntity.selectAll('.team')
+                .data(data)
+                .enter()
+                .append('a-sphere')
+                    .classed('team', true)
+                    .attr('color', (d)=> d.market == 'Michigan' ? color_theTeam : color_normalTeam) // here the color can be changed based on leage or something (maybe another scale is needed)
+                    .attr('scale', '0.1 0.1 0.1') // the scale can be changed based on Seed like `${0.1 * d.Seed} ${0.1 * d.Seed} ${0.1 * d.Seed}`
+                    .attr('position', (d) => {
+                        let teamUniqueName = getName(d),
+                            thisPosition = teamPlace(teams[teamUniqueName], d.points_game, d.tournament_round);
+                        // record the position for rival lines
+                            gameTeamIdPositions[d.id] = thisPosition;
+                        // record the position for self curves
+                            if (! teamsSelfPositions.hasOwnProperty(teamUniqueName)){
+                                teamsSelfPositions[teamUniqueName] = [thisPosition];
+                            } else {
+                                teamsSelfPositions[teamUniqueName].push(thisPosition);
+                            }
+
+                        return thisPosition;
+                    })
+                    .attr('event-set__mouseenter', function(d){ // a-frame way of on "mouseenter"
+                        return 'material.opacity: 0.5';
+                    })
+                    .attr('event-set__mouseleave', function(){ // a-frame way of on "mouseleave"
+                        return 'material.opacity: 1';
+                    })
+                    .on('mouseenter', function(d){
+                        console.log(d.points_game, " & ", getName(d), " & ", teams[getName(d)], " & ", d.tournament_round)
+                        drawTeamCurve(getName(d));
+                    })
+
+
+
+    // create the circles as round scales (the lower the score, the outer)
+        // set the lineweight
+            var torusLineWeight = 0.005,
+                ringLineWeight = 0.025;
+        // draw it the 3D way
+            aEntity.selectAll('.circleRuller')
                     .data(data)
                     .enter()
-                    .append('a-sphere')
-                        .classed('team', true)
-                        .attr('color', (d)=> d.market == 'Michigan' ? color_theTeam : color_normalTeam) // here the color can be changed based on leage or something (maybe another scale is needed)
-                        .attr('scale', '0.1 0.1 0.1') // the scale can be changed based on Seed like `${0.1 * d.Seed} ${0.1 * d.Seed} ${0.1 * d.Seed}`
-                        .attr('position', (d) => {
-                            let thisPosition = teamPlace(teams[getName(d)], d.points_game, d.tournament_round);
-                            gameTeamIdPositions[d.id] = thisPosition;
-                            return thisPosition;
-                        })
-                        .attr('event-set__mouseenter', function(d){ // a-frame way of on "mouseenter"
-                            return 'material.opacity: 0.5';
-                        })
-                        .attr('event-set__mouseleave', function(){ // a-frame way of on "mouseleave"
-                            return 'material.opacity: 1';
-                        })
-                        .on('mouseenter', function(d){ // debugging
-                            console.log(d.points_game, " & ", getName(d), " & ", teams[getName(d)], " & ", d.tournament_round)
-                        })
+                    .append('a-torus')
+                    .classed('circleRuller', true)
+                    .attr('color', color_circleScale)
+                    .attr('opacity', 0.1)
+                    .attr('rotation', '90 0 0')
+                    .attr('radius', (d) => r_scale(d.points_game))
+                    .attr('radius-tubular', torusLineWeight)
+                    .attr('segments-tubular', 100)
+                    .attr('position', (d) => `0 ${y_scale(d.tournament_round)} 0`)
+        // // or draw it the 2D way
+        //     aEntity.selectAll('.circleRuller')
+        //             .data(data)
+        //             .enter()
+        //             .append('a-ring')
+        //             .classed('circleRuller', true)
+        //             .attr('side', 'double')
+        //             .attr('color', color_circleScale)
+        //             .attr('opacity', 0.1)
+        //             .attr('rotation', '90 0 0')
+        //             .attr('radius-outer', (d) => r_scale(d.points_game) + ringLineWeight/2)
+        //             .attr('radius-inner', (d) => r_scale(d.points_game) - ringLineWeight/2)
+        //             .attr('segments-theta', 100)
+        //             .attr('position', (d) => `0 ${y_scale(d.tournament_round)} 0`)
+
+
+    // create the lines between the opposite teams
+        const data_firstHalf = data.slice(0, 67);
+        aEntity.selectAll('.rivalLine')
+                .data(data_firstHalf)
+                .enter()
+                .append('a-entity')
+                .classed('rivalLine', true)
+                .attr('line', (d)=>{
+                    //format: <a-entity line="start: 0, 1, 0; end: 2 0 -5; color: red"></a-entity>
+                    let thisTeamPosition = gameTeamIdPositions[d.id],
+                        thatTeamPosition = gameTeamIdPositions[+d.id + 67];
+                    return `start: ${thisTeamPosition}; end: ${thatTeamPosition}; color: ${color_oppositeLine}; opacity: 0.75`;
+                })
+
+
+    // create curves among the team itself of different rounds
+        // collection for all the a-curves
+            var curves = {}; // {teamUniqueNames: 0, teamUniqueNames: 1, ...} teamUniqueNames for teams that already been detected, 0 means it has only one point so no curve, 1 means it has a curve
+            var thisCurve;
+        // FUNCTION create the curves among the team itself at different rounds
+            const drawTeamCurve = (teamUniqueName) => {
+                console.log('selfCurve!');
+                console.log(teamUniqueName);
+
+                let curveId = `#${teamUniqueName}`;
+                // delete existing draw_curve
+                    d3.select('a-draw-curve').remove();
+                // if the team has been detected
+                    if (curves.hasOwnProperty(teamUniqueName)){
+                        // if the team has a curve, then draw it
+                            if (curves[teamUniqueName] == 1){
+                                aEntity.append('a-draw-curve')
+                                        .attr('material', `shader: line; color: ${color_selfCurve};`)
+                                        .attr('curveref', curveId);
+
+                                console.log("This team is in record, draw again!");
+                            } else{
+                                console.log("This team is in record, it 'sucks'...");
+                            }
+                    }
+                // if the team has not been detected, then let's detect
+                    else {
+                        let positions = teamsSelfPositions[teamUniqueName];
+                        console.log("positions this time: ",positions)
+                        // if the team has only one point, it will have no curve, so just record it
+                            if (positions.length == 1) {
+                                // record
+                                    curves[teamUniqueName] = 0;
+
+                                console.log("Have put this team in record, it 'sucks'...");
+                            }
+                        // else, record it, construct the curve, and then draw it
+                            else {
+                                // record
+                                    curves[teamUniqueName] = 1;
+                                // sort the positions array
+                                    positions.sort((a,b)=>{
+                                        var y_a = a.split(" ")[1],
+                                            y_b = b.split(" ")[1];
+                                        return y_a - y_b;
+                                    });
+                                // construct the curve from the array
+                                    thisCurve = aEntity.append('a-curve')
+                                                        .classed('selfCurve', true)
+                                                        .attr('id', teamUniqueName)
+                                // put each point as an a-curve-point into the a-curve
+                                // console.log(positions);
+                                    positions.forEach((p) => {
+                                        thisCurve.append('a-curve-point')
+                                                    .attr('position', p)
+
+                                    })
+                                // draw the curve
+                                    aEntity.append('a-draw-curve')
+                                            .attr('curveref', () => curveId)
+                                            .attr('material', `color: ${color_selfCurve};`)
+
+
+                                console.log("Have put this team in record, draw!");
+                            }
+                    }
+
+            }
+
+
+    // Draw a curve for real
+        var teamUniqueNames = Object.keys(teams);
+        // console.log(teamUniqueNames)
+        drawTeamCurve(teamUniqueNames[18]);
+
+    //NOTE:
+        //problem:
+            //the curve's position has an offset if it's on by hovering a point...
+            // try:
+                // the points for UMich if it's hovered:
+                                                        // 0: "-1.3557124072307212 -10 6.8156265248360075"
+                                                        // 1: "-1.2565139384089614 -5 6.3169221449699595"
+                                                        // 2: "-0.09919846882176019 0 0.49870437986604976"
+                                                        // 3: "-1.4549108760524814 5 7.314330904702057"
+                                                        // 4: "-1.091183157039361 10 5.485748178526543"
+                                                        // 5: "-1.3226462509568013 15 6.6493917315473245"
+                // the points for UMich if it's the first (drawTeamCurve(teamUniqueNames[18])):
+                                                        // 0: "-1.3557124072307212 -10 6.8156265248360075"
+                                                        // 1: "-1.2565139384089614 -5 6.3169221449699595"
+                                                        // 2: "-0.09919846882176019 0 0.49870437986604976"
+                                                        // 3: "-1.4549108760524814 5 7.314330904702057"
+                                                        // 4: "-1.091183157039361 10 5.485748178526543"
+                                                        // 5: "-1.3226462509568013 15 6.6493917315473245"
+            // so there is no problem.... wait to see why
 
 
 
-        // create the circles as round scales (the lower the score, the outer)
-            // set the lineweight
-                var torusLineWeight = 0.005,
-                    ringLineWeight = 0.025;
-            // draw it the 3D way
-                aEntity.selectAll('.circleRuller')
-                        .data(data)
-                        .enter()
-                        .append('a-torus')
-                        .classed('circleRuller', true)
-                        .attr('color', color_circleScale)
-                        .attr('opacity', 0.1)
-                        .attr('rotation', '90 0 0')
-                        .attr('radius', (d) => r_scale(d.points_game))
-                        .attr('radius-tubular', torusLineWeight)
-                        .attr('segments-tubular', 100)
-                        .attr('position', (d) => `0 ${y_scale(d.tournament_round)} 0`)
-            // // or draw it the 2D way
-            //     aEntity.selectAll('.circleRuller')
-            //             .data(data)
-            //             .enter()
-            //             .append('a-ring')
-            //             .classed('circleRuller', true)
-            //             .attr('side', 'double')
-            //             .attr('color', color_circleScale)
-            //             .attr('opacity', 0.1)
-            //             .attr('rotation', '90 0 0')
-            //             .attr('radius-outer', (d) => r_scale(d.points_game) + ringLineWeight/2)
-            //             .attr('radius-inner', (d) => r_scale(d.points_game) - ringLineWeight/2)
-            //             .attr('segments-theta', 100)
-            //             .attr('position', (d) => `0 ${y_scale(d.tournament_round)} 0`)
-
-
-        // create the lines between the opposite teams
-            const data_firstHalf = data.slice(0, 67);
-            aEntity.selectAll('.rivalLine')
-                    .data(data_firstHalf)
-                    .enter()
-                    .append('a-entity')
-                    .classed('rivalLine', true)
-                    .attr('line', (d)=>{
-                        //format: <a-entity line="start: 0, 1, 0; end: 2 0 -5; color: red"></a-entity>
-                        let thisTeamPosition = gameTeamIdPositions[d.id],
-                            thatTeamPosition = gameTeamIdPositions[+d.id + 67];
-                        return `start: ${thisTeamPosition}; end: ${thatTeamPosition}; color: ${color_oppositeLine}; opacity: 0.75`;
-                    })
-    })
+})
