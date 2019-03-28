a55290169e05328ed098c05cb774430140614d08
neilzhu2 <neilzhu@umich.edu>
2019-03-28 17:13:04 -0400

    text on left HUD

--- aFrame/js/script_for_aFrame_d3.js
+++ aFrame/js/script_for_aFrame_d3.js
@@ -10,31 +10,12 @@
 
     var hud_width = 0.5,
         hud_height = 1,
-        hud_position_x = 1.65,
-        hud_position_z = -1.5,
+        hud_position_x = 1.5,
+        hud_position_z = -1.6,
         hud_rotation = 15,
         hud_color = '#B2CFE8',
         hud_opacity_hover = 0.2,
         hud_opacity_selected = 0.6;
-
-    var hud_hover = aHead.append('a-entity')
-                            //.attr('id','hud_hover')
-                            .attr('geometry',`primitive: plane; height: ${hud_height}; width: ${hud_width}`)
-                            .attr('position',`${hud_position_x} 0 ${hud_position_z}`) //X,Y,Z
-                            .attr('rotation', `0 -${hud_rotation} 0`)
-                            .attr('material',`color: ${hud_color}; transparent: true; opacity: ${hud_opacity_hover}`)
-                            .attr('text',`color: white; align: center; value: hover; width: ${hud_width}`);
-    var hud_selected = aHead.append('a-entity')
-                            //.attr('id','hud_selected')
-                            .attr('geometry',`primitive: plane; height: ${hud_height}; width: ${hud_width}`)
-                            .attr('position',`-${hud_position_x} 0 ${hud_position_z}`) //X,Y,Z
-                            .attr('rotation', `0 ${hud_rotation} 0`)
-                            .attr('material',`color: ${hud_color}; transparent: true; opacity: ${hud_opacity_selected}`)
-                            .attr('text',`color: white; align: center; value: select team to compare; width: ${hud_width}`);
-    var svg = hud_selected.append('a-curve')
-
-
-
 
 // independent parameters
     const yInterval_initial = -10,
@@ -45,13 +26,13 @@
           minX = - (hud_width * 0.5) * 0.9
           maxX = (hud_width * 0.5) * 0.9,
           heights = [-15, -10, -5, 0, 5, 10, 15]; // the actual heights of each round in the VR
-          heights_hud = [-(hud_height * 0.5) * 0.9, -(hud_height * 0.5) * 0.6, -(hud_height * 0.5) * 0.3, 0, (hud_height * 0.5) * 0.3, (hud_height * 0.5) * 0.6, (hud_height * 0.5) * 0.9]; // the heights of the y for rounds on the HUD
+          heights_hud = [-(hud_height * 0.5) * 0.6, -(hud_height * 0.5) * 0.4, -(hud_height * 0.5) * 0.2, 0, (hud_height * 0.5) * 0.2, (hud_height * 0.5) * 0.4, (hud_height * 0.5) * 0.6]; // the heights of the y for rounds on the HUD
 
     //colors assignment
         const color_Michigan = '#76AAFF',
               color_winner = '#FFEC04',
               color_loser = '#FFE98A',
-              color_hoveredTeam = 'white',
+              color_hoveredTeam = 'red',
               color_circleScale = 'white',
               color_oppositeLine = 'red',
               color_selfCurve = 'green',
@@ -69,9 +50,39 @@
 // FUNCTION to get the uniquename of a team
     const getName = (data) => {
         let usableMarketName = data.market.replace(' ', '_'),
-            usableTeamName =  data.name.replace(' ', '_')
+            usableTeamName =  data.name.replace(' ', '_');
+            usableMarketName = usableMarketName.replace('&','_and_');
+
         return usableMarketName + '_' + usableTeamName;
     }
+
+
+
+// Create hud
+    var hud_hover = aHead.append('a-entity')
+                            //.attr('id','hud_hover')
+                            .attr('geometry',`primitive: plane; height: ${hud_height}; width: ${hud_width}`)
+                            .attr('position',`${hud_position_x + hud_width/2} 0 ${hud_position_z}`) //X,Y,Z
+                            .attr('rotation', `0 -${hud_rotation} 0`)
+                            .attr('material',`color: ${hud_color}; transparent: true; opacity: ${hud_opacity_hover}`)
+                            .attr('text',`color: white; align: center; value: hover; width: ${hud_width}`);
+    var hud_selected = aHead.append('a-entity')
+                            //.attr('id','hud_selected')
+                            .attr('geometry',`primitive: plane; height: ${hud_height}; width: ${hud_width}`)
+                            .attr('position',`-${hud_position_x  + hud_width/2} 0 ${hud_position_z}`) //X,Y,Z
+                            .attr('rotation', `0 ${hud_rotation} 0`)
+                            .attr('material',`color: ${hud_color}; transparent: true; opacity: ${hud_opacity_selected}`)
+                            .attr('text',`color: white; align: center; value: select team to compare; width: ${hud_width}`);
+        var hud_MinMaxLabel = hud_selected.append('a-entity')
+                                            .attr('position', `0 -0.025 0`);
+            var hud_MinMaxLabel_min = hud_MinMaxLabel.append('a-entity')
+                                                    .attr('geometry', 'primitive: plane; height: 0.02; width: 0.002' )
+                                                    .attr('material', 'color: white')
+                                                    .attr('position', `${minX} ${hud_height / 2 * 0.05} 0`)
+            var hud_MinMaxLabel_max = hud_MinMaxLabel.append('a-entity')
+                                                    .attr('geometry', 'primitive: plane; height: 0.02; width: 0.002' )
+                                                    .attr('material', 'color: white')
+                                                    .attr('position', `${maxX} ${hud_height / 2 * 0.05} 0`)
 
 
 
@@ -96,6 +107,16 @@ d3.csv('2017_season_detailed_cleaned.csv').then(function(data){
     // parameters that need csv
     const maxPoint = Math.max(...points),
           minPoint = Math.min(...points);
+
+          //put this information to the label
+              hud_MinMaxLabel_min.append('a-entity')
+                                  .attr('rotation', '0 0 -90')
+                                  .attr('position', `0 -0.3 0`)
+                                  .attr('text', `color: white; value: ${minPoint}; width: 0.5; `);
+              hud_MinMaxLabel_max.append('a-entity')
+                                  .attr('rotation', '0 0 -90')
+                                  .attr('position', `0 -0.3 0`)
+                                  .attr('text', `color: white; value: ${maxPoint}; width: 0.5;`);
 
     const r_scale = d3.scaleLinear()
                           .domain([minPoint, maxPoint])
@@ -131,23 +152,8 @@ d3.csv('2017_season_detailed_cleaned.csv').then(function(data){
             const drawTeamCurve = (teamUniqueName) => {
                 let curveId = `#${teamUniqueName}`,
                     curveId_hud = `#${teamUniqueName}_hud`
-                // if the team has been detected
-                    if (curves.hasOwnProperty(teamUniqueName)){
-                        // // if the team has a curve, then draw it
-                        //     if (curves[teamUniqueName] == 1){  // curve is {teamUniqueNames: 0, teamUniqueNames: 1, ...}. teamUniqueNames for teams that already been detected, 0 means it has only one point so no curve, 1 means it has a curve
-                        //         aEntity.append('a-draw-curve')
-                        //                 .attr('material', `shader: line; color: ${color_selfCurve};`)
-                        //                 .attr('curveref', curveId);
-                        //         hud_selected.append('a-draw-curve')
-                        //                     .attr('material', `shader: line; color: ${color_selfCurve};`)
-                        //                     .attr('curveref', curveId_hud);
-                        //         // console.log("This team is in record, draw again!");
-                        //     } else{
-                        //         // console.log("This team is in record, but it does not make any progress in the tournament...");
-                        //     }
-                    }
                 // if the team has not been detected, then let's detect
-                    else {
+                    if (! curves.hasOwnProperty(teamUniqueName)) {
                         let positions = teamsSelfPositions[teamUniqueName],  // {teamName:[points], ...}, this is generated when the node was generated
                             positions_hud = teamsSelfPositions_hud[teamUniqueName];
                         // if the team has only one point, it will have no curve, so just record it
@@ -195,7 +201,12 @@ d3.csv('2017_season_detailed_cleaned.csv').then(function(data){
                                     hud_selected.append('a-draw-curve')
                                                 .attr('id', `${teamUniqueName}Curve_hud`)
                                                 .attr('curveref', () => curveId_hud)
-                                                .attr('material', `color: ${color_selfCurve}; opacity: 0.0`)
+                                                .attr('material', `color: ${color_selfCurve_hud}; opacity: 0.0`)
+                                    hud_selected.append('a-entity')
+                                                .attr('id', `${teamUniqueName}CurveText_hud`)
+                                                .attr('text', `value: ${teamUniqueName}; color: ${color_selfCurve_hud}; opacity: 0.0; width: 0.4; anchor: left`)
+                                                .attr('rotation', '0 0 -90')
+                                                .attr('position', `${positions_hud[0].split(" ")[0]} ${positions_hud[0].split(" ")[1]} 0`)
                             }
                     }
             }
@@ -237,12 +248,6 @@ d3.csv('2017_season_detailed_cleaned.csv').then(function(data){
                             }
                         return thisPosition;
                     })
-                    // .attr('event-set__mouseenter', function(d){ // a-frame way of on "mouseenter"
-                    //     return 'material.opacity: 0.5';
-                    // })
-                    // .attr('event-set__mouseleave', function(){ // a-frame way of on "mouseleave"
-                    //     return 'material.opacity: 1';
-                    // })
                     .on('mouseenter', function(d){
                         // change this node's style
                             this.setAttribute('opacity', '0.5');
@@ -303,6 +308,7 @@ d3.csv('2017_season_detailed_cleaned.csv').then(function(data){
                             currentCurveOpacity = selectedCurve.getAttribute('material').opacity,
 
                             selectedCurve_hud = document.querySelector(`#${teamUniqueName}Curve_hud`),
+                            selectedCurveText_hud = document.querySelector(`#${teamUniqueName}CurveText_hud`),
                             currentCurveOpacity_hud = selectedCurve_hud.getAttribute('material').opacity;
 
                         // emphasize or unemphasize the curve in 3D
@@ -315,17 +321,19 @@ d3.csv('2017_season_detailed_cleaned.csv').then(function(data){
                         //show or hide the curve on selected panel
                             if(currentCurveOpacity_hud == 0.0) {
                                 selectedCurve_hud.setAttribute('material', `color: ${color_selfCurve_hud}; opacity: 1`);
+                                selectedCurveText_hud.setAttribute('text','opacity', 1);
                                 teamsShownOnHud.push(teamUniqueName);
-                                hud_selected.attr('text',`color: white; align: center; value:  ; width: ${hud_width}`)
+                                hud_selected.attr('text',`color: white; align: center; value:  ; width: ${hud_width}`);
                             } else {
                                 selectedCurve_hud.setAttribute('material', `color: ${color_selfCurve_hud}; opacity: 0.0`);
+                                selectedCurveText_hud.setAttribute('text','opacity', 0);
                                 for(let i = 0; i < teamsShownOnHud.length; i ++){
                                     if(teamsShownOnHud[i] == teamUniqueName) {
                                         teamsShownOnHud.splice(i, 1)
                                     };
                                 }
                                 if(teamsShownOnHud.length == 0) {
-                                    hud_selected.attr('text',`color: white; align: center; value: select team to compare; width: ${hud_width}`)
+                                    hud_selected.attr('text',`color: white; align: center; value: select team to compare; width: ${hud_width}`);
                                 }
                             };
 
